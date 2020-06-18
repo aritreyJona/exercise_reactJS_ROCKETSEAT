b9e8f79f6526e4b4811991873a4323b2b155d2de
aritreyJona <51917383+aritreyJona@users.noreply.github.com>
2020-06-18 20:25:34 -0300

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
-import Repo from "./components/Repo";
 import api from "./services/api";
 
 function App() {
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState([{ title: "hallo", id: "world" }]);
 
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post(`/repositories`, {
+      title: "new Title",
+      url: "www.johanna.de",
+      techs: ["my", "list"],
+    });
+    console.log(response.data);
+    if (response.status === 200) {
+      setRepos([...repos, response.data]);
+    } else console.log("there was a problem while creating a new repo");
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+    if (response.status != 204) {
+      alert("something went wrong");
+    } else {
+      const newRepos = [];
+      repos.map((repo) => (repo.id != id ? newRepos.push(repo) : null));
+      setRepos(newRepos);
+    }
   }
 
-  async function getRepos() {}
+  useEffect(() => {
+    api.get("/repositories").then((response) => setRepos(response.data));
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <Repo onPress={handleRemoveRepository(1)} />
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
